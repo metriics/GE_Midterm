490edfe578c93eba18d2a296bf88e48a873e3d5d
Steph <stephane.gagnon00@gmail.com>
2020-10-05 22:05:28 -0400

    working first person camera + start of trigger system

---
+++ Assets/_Scripts/MazeLogic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MazeLogic : MonoBehaviour
+{
+    // references
+    [SerializeField]
+    public Text remainingTimeText;
+    float secondsRemaining;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        secondsRemaining = 60.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (secondsRemaining > 0.0f)
+        {
+            secondsRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            // save score here, kick to stats menu
+            SceneManager.LoadScene("End Scene");
+        }
+
+        // round seconds from float to int, convert to string
+        remainingTimeText.text = "Find a checkpoint in less than " + Mathf.RoundToInt(secondsRemaining).ToString() + " seconds.";
+
+
+    }
+}
---
+++ Assets/_Scripts/Movement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    float mouseX;
+    float mouseY;
+    float posX;
+    float posZ;
+    float posY;
+    public float mouseSensitivity = 500;
+    public float speed = 15.0f;
+    Collider playerCollider;
+    public Collider checkpoint1Collider;
+
+    bool cursorVisible = false;
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hideCursor();
+        playerCollider = this.GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // show cursor when game paused
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            cursorVisible = !cursorVisible;
+            if (cursorVisible) // game paused
+            {
+                showCursor();
+            }
+            else // game unpaused
+            {
+                hideCursor();
+            }
+        }
+
+        if (!cursorVisible) // game unpasued
+        {
+            // first person camera
+            mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            yRotation += mouseX;
+            xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+
+            transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0.0f);
+
+            // trigger collisions
+            
+        }
+    }
+
+    void hideCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        cursorVisible = false;
+    }
+
+    void showCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        cursorVisible = true;
+    }
+}
