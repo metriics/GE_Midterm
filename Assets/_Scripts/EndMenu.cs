4674b824ad36667714ca218763f3756271ba6743
Steph <stephane.gagnon00@gmail.com>
2020-10-05 20:09:12 -0400

    Working scene management + menu system

---
+++ Assets/_Scripts/EndMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndMenu : MonoBehaviour
+{
+    // references
+    [SerializeField]
+    public GameObject mainMenuButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // we can use this later to load in stats from previous sessions
+    }
+
+    public void sceneMenu()
+    {
+        SceneManager.LoadScene("Start Scene");
+    }
+}
---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // references
+    [SerializeField]
+    public GameObject playButton;
+    public GameObject statsButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // we can use this later to load in stats from previous sessions
+    }
+
+    public void scenePlay()
+    {
+        SceneManager.LoadScene("Play Scene");
+    }
+
+    public void sceneStats()
+    {
+        SceneManager.LoadScene("End Scene");
+    }
+}
